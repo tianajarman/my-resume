c560f5435c5ed30abf82a3c1750943271891fac7
Tiana Jarman <tjarman@udel.edu>
2017-11-14 14:12:32 -0500

    fixed modal

--- resume.py
+++ resume.py
@@ -10,13 +10,13 @@ def home():
 @app.route('/courses')
 def courses():
     courses = [
-        ['MISY 225', 'Introduction to Programming Business Applications',
+        ['MISY225', 'Introduction to Programming Business Applications',
             'Learning to program in Java.'],
-        ['MISY 330', 'Database Design & Implementation',
+        ['MISY330', 'Database Design & Implementation',
             'Learning data mining alogorithms and the SQL language'],
-        ['CISC 355', 'Computers, Ethics & Society',
+        ['CISC355', 'Computers, Ethics & Society',
             'Analyzing the impact of computers on our society.'],
-        ['MISY 350', 'Web Application Development',
+        ['MISY350', 'Web Application Development',
             'Fundamentals of web development, including Python and Flask.']
     ]
     return render_template('courses.html', courses=courses)
